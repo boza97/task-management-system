d2a9d33ee085d375b955430cbec7f17d75c28522
Bozidar Mastilovic <bozidar.mastilovicc@gmail.com>
2026-02-16 11:07:21 +0100

    update sidebar component

--- frontend/task-managment-system/src/app/layout/sidebar/sidebar/sidebar.ts
+++ frontend/task-managment-system/src/app/layout/sidebar/sidebar/sidebar.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [RouterLinkActive, RouterLink],
   templateUrl: './sidebar.html',
-  styleUrl: './sidebar.scss'
+  styleUrl: './sidebar.scss',
 })
-export class Sidebar {
-
-}
+export class Sidebar {}
