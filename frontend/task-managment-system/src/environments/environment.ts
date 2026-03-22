6cc01f88b5de40b038d667ba6c385efde582b458
Bozidar Mastilovic <bozidar.mastilovicc@gmail.com>
2026-03-23 00:43:05 +0100

    update api endpoint

--- frontend/task-managment-system/src/environments/environment.ts
+++ frontend/task-managment-system/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://tms-backend.azurewebsites.net/api',
+  apiUrl: 'https://tms-backend-a7bxffdedjbacfcy.francecentral-01.azurewebsites.net/api',
 };
